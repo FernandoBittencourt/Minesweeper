ff0a39ef3d2658342b06cf7dd042ea637ecd5207
FernandoBittencourt <fernandodbittencourt@hotmail.com>
2026-01-21 20:52:50 -0300

    command-line interface updated

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Minesweeper minesweeper = new Minesweeper(20, 10, 15);
+        Minesweeper minesweeper = new Minesweeper(10, 10, 5);
+        System.out.println(minesweeper.draw());
         while(GameState.RUNNING.equals(minesweeper.getState())) {
-            System.out.println(minesweeper.draw());
             System.out.print("Enter row and column (e.g., 0 1): ");
             int row = sc.nextInt();
             int col = sc.nextInt();
-            minesweeper.reveal(row, col);
+            try {
+                minesweeper.reveal(row, col);
+                System.out.println(minesweeper.draw());
+            } catch (IllegalArgumentException ex) {
+                System.out.println(ex.getMessage());
+            }
         }
         System.out.println(minesweeper.draw());
         System.out.println("Game " + minesweeper.getState());
